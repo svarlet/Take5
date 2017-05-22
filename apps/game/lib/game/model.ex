58929c7cb95cc5b13b610b9cc51f5e3d14c4ff03
Sébastien Varlet <sebastien.varlet@gmail.com>
2017-05-22 13:52:33 +0100

    Initialize correctly the deck.

--- apps/game/lib/game/model.ex
+++ apps/game/lib/game/model.ex
@@ -1,5 +1,13 @@
 defmodule Game.Model do
-  @deck List.duplicate 0, 104
+  @deck (for n <- 1..104, into: %{} do
+          cond do
+            n == 55 -> {55, 7}
+            rem(n, 5) == 0 && rem(n, 10) != 0 -> {n, 2}
+            rem(n, 11) == 0 -> {n, 5}
+            rem(n, 10) == 0 -> {n, 3}
+            true -> {n, 1}
+          end
+        end)
 
   defstruct players: [], hands: [], deck: @deck
 
--- apps/game/test/game/model_test.exs
+++ apps/game/test/game/model_test.exs
@@ -27,7 +27,27 @@ defmodule Game.ModelTest do
 
     test "a deck of 104 cards" do
       model = %Model{}
-      assert length(model.deck) == 104
+      assert Enum.count(model.deck) == 104
+
+      number_for_penalties =
+        model.deck
+        |> Enum.group_by(fn {_, penalty} -> penalty end, fn {number, _} -> number end)
+
+      penalty_of = fn penalty ->
+        number_for_penalties
+        |> Map.get(penalty)
+        |> Enum.sort
+      end
+
+      assert penalty_of.(7) == [55]
+      assert penalty_of.(5) == [11, 22, 33, 44, 66, 77, 88, 99]
+      assert penalty_of.(3) == [10, 20 , 30 , 40 , 50 , 60, 70, 80, 90, 100]
+      assert penalty_of.(2) == [5, 15, 25, 35, 45, 65, 75, 85, 95]
+      assert penalty_of.(1) == [1, 2, 3, 4, 6, 7, 8, 9, 12, 13, 14, 16, 17, 18, 19, 21, 23, 24, 26, 27, 28, 29,
+                                31, 32, 34, 36, 37, 38, 39, 41, 42, 43, 46, 47, 48, 49, 51, 52, 53, 54, 56, 57,
+                                58, 59, 61, 62, 63, 64, 67, 68, 69, 71, 72, 73, 74, 76, 78, 79, 81, 82, 83, 84, 86, 87,
+                                89, 91, 92, 93, 94, 96, 97, 98, 101, 102, 103, 104]
     end
+
   end
 end

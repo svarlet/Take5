ca770e288e394f60ae7679d9b0c5657b29204ad3
Sébastien Varlet <sebastien.varlet@gmail.com>
2017-05-24 12:18:39 +0100

    Remove duplication of Model creations from the model module test.

--- apps/game/test/game/model_test.exs
+++ apps/game/test/game/model_test.exs
@@ -4,6 +4,10 @@ defmodule Game.ModelTest do
   alias Game.Model
   alias Game.Model.Card
 
+  defp create_model(_) do
+    [model: %Model{}]
+  end
+
   describe "cards" do
     test "are numbered" do
       assert Map.has_key?(%Card{}, :number)
@@ -15,27 +19,25 @@ defmodule Game.ModelTest do
   end
 
   describe "a model is initialized with" do
-    test "an empty list of players" do
-      model = %Model{}
-      assert model.players == []
+    setup [:create_model]
+
+    test "an empty list of players", context do
+      assert context.model.players == []
     end
 
-    test "an empty list of hands" do
-      model = %Model{}
-      assert model.hands == []
+    test "an empty list of hands", context do
+      assert context.model.hands == []
     end
 
-    test "an empty table" do
-      model = %Model{}
-      assert model.table == []
+    test "an empty table", context do
+      assert context.model.table == []
     end
 
-    test "a deck of 104 unique cards with penalties" do
-      model = %Model{}
-      assert Enum.count(model.deck) == 104
+    test "a deck of 104 unique cards with penalties", context do
+      assert Enum.count(context.model.deck) == 104
 
       number_for_penalties =
-        model.deck
+        context.model.deck
         |> Enum.group_by(fn {_, penalty} -> penalty end, fn {number, _} -> number end)
 
       penalty_of = fn penalty ->
@@ -53,6 +55,6 @@ defmodule Game.ModelTest do
                                 58, 59, 61, 62, 63, 64, 67, 68, 69, 71, 72, 73, 74, 76, 78, 79, 81, 82, 83, 84, 86, 87,
                                 89, 91, 92, 93, 94, 96, 97, 98, 101, 102, 103, 104]
     end
-
   end
+
 end

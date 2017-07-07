43e56ad540f9c44f3e56794ff17a0ba5b420e751
Sébastien Varlet <sebastien.varlet@gmail.com>
2017-07-07 18:05:14 +0100

    Ensure that a game requires 2 to 10 player names to be created.

--- apps/game/lib/game.ex
+++ apps/game/lib/game.ex
@@ -1,2 +1,12 @@
 defmodule Game do
+
+  alias Game.Player
+
+  def new(player_names) when length(player_names) in 2..10 do
+    Enum.map(player_names, &Player.new/1)
+  end
+
+  def players(game) do
+    game
+  end
 end
--- apps/game/test/game_test.exs
+++ apps/game/test/game_test.exs
@@ -1,7 +1,36 @@
 defmodule GameTest do
   use ExUnit.Case, async: true
+  use PropCheck
+
+  import TestHelper
+
   doctest Game.Deck
   doctest Game.Card
   doctest Game.Player
   doctest Game.Table
+
+  property "a game cannot be created with less than 2 players or more than 10 players", [:verbose] do
+    forall count <- union([integer(0, 1), integer(11, :inf)]) do
+      forall names <- vector(count, player_name_gen()) do
+        try do
+          Game.new(names)
+        rescue
+          FunctionClauseError -> true
+        else
+          _ -> false
+        end
+      end
+    end
+  end
+
+  property "a game is initialized with 2 to 10 players", [:verbose] do
+    forall names <- list(player_name_gen()) do
+      implies Enum.count(names) in 2..10 do
+        names == names
+        |> Game.new()
+        |> Game.players()
+        |> Enum.map(fn p -> p.name end)
+      end
+    end
+  end
 end

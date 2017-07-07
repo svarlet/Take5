57d69567bf89254f622294b1d9fa48d31a8f4b93
Sébastien Varlet <sebastien.varlet@gmail.com>
2017-07-07 17:38:32 +0100

    Test that a new deck has 104 unique cards.

--- apps/game/test/game/deck_test.exs
+++ apps/game/test/game/deck_test.exs
@@ -2,5 +2,16 @@ defmodule Game.DeckTest do
   use ExUnit.Case, async: true
   use PropCheck
 
+  alias Game.Deck
+
+  test "a new deck has 104 cards" do
+    assert 104 == Enum.count(Deck.deck())
+  end
+
+  test "a new deck has all heads from 1 to 104" do
+    assert Enum.to_list(1..104) == Deck.deck()
+    |> Enum.map(fn c -> c.head end)
+  end
+
 
 end
